f27cd51c70130f9dad86ba23877063e4ec93b48c
Brandon Weaver <a7xncob@gmail.com>
2019-11-06 03:14:25 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
+  index = 0
+  return_values = []
+  while index < collection.size do
+    return_values << yield(collection[index])
+    index += 1
+  end
+  
+  if return_values.include?(false)
+    return false
+  else
+    return true
+  end
 end
